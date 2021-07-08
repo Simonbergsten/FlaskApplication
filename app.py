aa84c40541f41f60aa32d95d01fd086b9aefb0ec
simon <simon.bergsten@hotmail.com>
2021-07-08 10:45:57 +0200

    updated app.py in order to use postgreSQL

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask
 from flask_restful import Api
@@ -13,7 +14,11 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri = os.environ.get("DATABASE_URL", 'sqlite:///data.db')  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
@@ -29,6 +34,7 @@ api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
+
     db.init_app(app)
 
     if app.config['DEBUG']:
